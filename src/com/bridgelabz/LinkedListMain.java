09556d98ccc3740801449faeb28cff4532a32053
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-08-07 16:44:38 +0530

    Added:Linked List

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -12,5 +12,14 @@ public class LinkedListMain {
         linkedList.push(70);
         linkedList.display();
 
+        /*
+         UC2: Simple Linked list by pushing 70,30,56.
+         */
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        linkedList1.push(70);
+        linkedList1.push(30);
+        linkedList1.push(56);
+        linkedList1.display();
     }
+
 }
